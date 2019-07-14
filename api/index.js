0367e6082d62d35e95896ebd5a6b332662630bd1
utakt <bokuhakokoniiru@gmail.com>
2019-07-14 16:09:31 +0900

    初回コミット

---
+++ api/index.js
@@ -0,0 +1,7 @@
+const app = require('express')()
+module.exports = { path: '/api', handler: app }
+
+app.get('/hello', (req, res) => {
+    console.log('hello nuxt in text')
+    res.send('world')
+})
